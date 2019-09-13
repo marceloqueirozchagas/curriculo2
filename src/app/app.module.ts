4788feb004f0292092c0408a5f0c755bf2922b58
mqchagas <mqchagas@fazenda.ms.gov.br>
2019-09-13 14:14:05 -0400

    criado os components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { MytabsComponent } from './mytabs/mytabs.component';
+import { FeatureComponent } from './feature/feature.component';
+import { GaleryComponent } from './galery/galery.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WelcomeComponent,
+    MytabsComponent,
+    FeatureComponent,
+    GaleryComponent,
+    TestimonialsComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mytabs/mytabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mytabs',
+  templateUrl: './mytabs.component.html',
+  styleUrls: ['./mytabs.component.css']
+})
+export class MytabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
