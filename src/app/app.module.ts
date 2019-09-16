ab31cdbd827fb59b34a5ba34f64a45083cf607d8
mqchagas <mqchagas@fazenda.ms.gov.br>
2019-09-16 13:38:17 -0400

    efetuado limpeza de arquivos não utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { MytabsComponent } from './mytabs/mytabs.component';
-import { FeatureComponent } from './feature/feature.component';
-import { GaleryComponent } from './galery/galery.component';
-import { TestimonialsComponent } from './testimonials/testimonials.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     HomeComponent,
-    WelcomeComponent,
-    MytabsComponent,
-    FeatureComponent,
-    GaleryComponent,
-    TestimonialsComponent,
-    FooterComponent,
-    HeaderComponent
+    AboutComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
